acf2cb93aacca6fee58351281e1f50b598dd9ba5
tefiiyy <00001120744520sp@al.educacao.sp.gov.br>
2024-08-09 18:41:56 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,31 @@
+let cor;
+let cx;
+let cy;
+
+function setup() {
+  createCanvas(600, 600);
+  background("rgb(224,58,87)");
+  cor = color(random(0,255),random(0,255),random(0,255));
+  cx = [0,0,0];
+  cy = [random(height),random(height),random(height)];
+}
+
+function draw() {
+  fill(cor);
+  
+  for(let quantos in cx){
+    circle(cx[quantos],cy[quantos],70);
+    cx[quantos]+= random(0,3)
+    cx[quantos]+= random(-3,3)
+    if(cx[quantos] >= width){
+      cx[quantos] =0;
+      cy[quantos] = random(height)
+    }
+  }
+  
+  if(mouseIsPressed){
+    cor = color(random(0,255),random(0,255),random(0,255),random(0,100));
+    
+  }
+  
+}
